1a1e7a14bf93c20955efc43e5507cf1688d76894
EmiinaPeljto <emina.peljto@stu.ibu.edu.ba>
2025-07-23 22:15:04 +0200

    :(

--- backend/config/rapidapi.js
+++ backend/config/rapidapi.js
@@ -4,4 +4,5 @@ module.exports = {
   rapidapi_key: process.env.RAPIDAPI_KEY,
   rapidapi_host: process.env.RAPIDAPI_HOST,
   x_rapidapi_host: process.env.IMAGE_RAPID_HOST,
+  image_key: process.env.IMAGE_API,
 };
